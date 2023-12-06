6aceca9cb2219330eb4f05076c3dddac8d395113
VictorLyalchuk <121404620+VictorLyalchuk@users.noreply.github.com>
2023-12-06 20:02:21 +0200

    base

---
+++ config/connection_database.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$dbname = "pv116";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
--- index.php
+++ index.php
@@ -11,7 +11,10 @@
 </head>
 <body>
 <div class="container">
-    <?php include ("_header.php")?>
+    <?php include ("_header.php");
+    include $_SERVER['DOCUMENT_ROOT'] . "/config/connection_database.php";
+    ?>
+
 
     <h1 class ="text-center">Caterogies</h1>
     <?php
@@ -34,18 +37,27 @@
             <th scope="col">#</th>
             <th scope="col">Photo</th>
             <th scope="col">Name</th>
+            <th scope="col">Description</th>
             <th scope="col"></th>
         </tr>
         </thead>
         <tbody>
         <?php
-        for ($a=0; $a < $n; $a++)  {?>
+        $sql = "SELECT id, name, image, description FROM categories";
+        $stmt = $pdo->query($sql);
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($results as $row)
+        {
+
+        ?>
         <tr>
-            <th scope="row"><?php echo $list[$a]["id"] ?></th>
+            <th scope="row"><?php echo $row["id"] ?></th>
             <td>
-                <img src = "<?php echo $list[$a]["image"] ?>" height ="75" alt="Photo">
+                <img src = "<?php echo $row["image"] ?>" height ="75" alt="Photo">
             </td>
-            <td><?php echo $list[$a]["name"] ?></td>
+            <td><?php echo $row["name"] ?></td>
+            <td><?php echo $row["description"] ?></td>
             <td>
                 <a href="#" class="btn btn-info">View</a>
             </td>
